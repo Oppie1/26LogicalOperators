85f3c738e982417f249b6fb240aa323f5314a51f
Oppie1 <atindall729@gmail.com>
2025-01-06 16:19:06 -0600

    Add files via upload

---
+++ 26LogicalOperators/LogicalOperators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int age = 20;
+	int money = 600;
+
+	if (age > 21 || money>500) {
+			cout << "You're allowed in!";
+		}
+	}
